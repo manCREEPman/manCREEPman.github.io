4649de854098f67b13c84c851e7781bf45d25e86
manCREEPman <barbekyan@yandex.ru>
2021-01-30 01:14:29 +0300

    Новая потуга

--- README.md
+++
@@ -1,1 +0,0 @@
-# manCREEPman.github.io
--- main_page_script.js
+++
@@ -1,5 +0,0 @@
-function checkbox_spawn_frequency(){
-    let container = document.getElementById("checkbox-spawn-frequency");
-    let labelInputHTML = "<label class=\"main-text-size\" for=\"spawn-rate\">Частота появления</label>";
-    labelInputHTML += "\n <input type=\"range\" "
-}
